ebd4dabb630d97a571b0880d1125569802d33899
Matko Smoljan <matko.smoljan2@gmail.com>
2022-10-29 12:27:49 +0200

    Reapply gitignore

--- src/waffle_solver/factories.clj
+++ src/waffle_solver/factories.clj
@@ -1,11 +1,18 @@
 (ns waffle-solver.factories
+  (:require [clojure.string :refer [upper-case]])
+  (:require [waffle-solver.game-types :refer [game-types]])
   (:gen-class))
 
 (defn create-letter
   "Creates an empty letter"
   [letter type]
-  {:pre [
-    (re-matches #"[A-Za-z\.]" letter)
-    (some #{type} [:correct :maybe :incorrect])
-  ]}
-  {:letter (clojure.string/upper-case letter) :type type})
+  {:pre [(re-matches #"[A-Za-z\.]" letter)
+         (some #{type} [:correct :maybe :incorrect])]}
+  {:letter (upper-case letter) :type type})
+
+(defn create-game "Creates an empty game board"
+  ([] (create-game :waffle))
+  ([type]
+  {:pre (contains? game-types type)}
+  (let [element-count (get game-types type)]
+    (repeat element-count (create-letter "." :incorrect)))))
---
+++ src/waffle_solver/game_types.clj
@@ -0,0 +1,6 @@
+(ns waffle-solver.game-types
+  (:gen-class))
+
+(def game-types
+  {:waffle        21
+   :deluxe-waffle 40})
