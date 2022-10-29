59fc7741b57a48db48d570bba243008183e200c3
Matko Smoljan <matko.smoljan2@gmail.com>
2022-10-29 20:12:06 +0200

    Minor formatting

--- src/waffle_solver/factories.clj
+++ src/waffle_solver/factories.clj
@@ -1,6 +1,6 @@
 (ns waffle-solver.factories
-  (:require [clojure.string :refer [upper-case]])
-  (:require [waffle-solver.game-types :refer [game-types]])
+  (:require [clojure.string :refer [upper-case]]
+            [waffle-solver.game-types :refer [game-types]])
   (:gen-class))
 
 (defn create-square
@@ -14,6 +14,6 @@
   "Creates an empty game board"
   ([] (create-game :waffle))
   ([type]
-  {:pre (contains? game-types type)}
-  (let [element-count (get-in game-types [type :total-squares])]
-    (repeat element-count (create-square "." :incorrect)))))
+   {:pre (contains? game-types type)}
+   (let [element-count (get-in game-types [type :total-squares])]
+     (repeat element-count (create-square "." :incorrect)))))
