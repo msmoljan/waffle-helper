93eb0a19950b997ab46700a3d3233d641c588960
Matko Smoljan <matko.smoljan2@gmail.com>
2022-10-30 15:55:08 +0100

    Update example

--- src/waffle_solver/core.clj
+++ src/waffle_solver/core.clj
@@ -6,14 +6,19 @@
 
 (defn- generate-example-game []
   (let
-    [game (create-game :waffle)
-     squares (vec (get game :squares))]
-    (let
-      [updated-squares (assoc squares
-                         0 {:letter "A", :type :correct}
-                         4 {:letter "T", :type :correct}
-                         6 {:letter "E", :type :maybe})]
-      (assoc game :squares updated-squares))))
+    [game (create-game :deluxe-waffle)
+     squares (assoc (vec (get game :squares))
+               0 {:letter "P", :type :correct}
+               1 {:letter "A", :type :correct}
+               2 {:letter "E", :type :maybe}
+               3 {:letter "R", :type :maybe}
+               4 {:letter "N", :type :correct}
+               5 {:letter "T", :type :correct}
+               6 {:letter "S", :type :correct}
+               8 {:letter "E", :type :maybe}
+               23 {:letter "C", :type :maybe}
+               39 {:letter "T", :type :correct})]
+    (assoc game :squares squares)))
 
 (defn -main
   [& args]
